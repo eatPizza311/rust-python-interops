5a1957868200634602186597119d8a14bed3c6c8
ChihYing <spacetime0311@gmail.com>
2025-02-23 17:51:06 +0800

    feat: complete 08_outro

--- exercises/01_intro/08_outro/src/lib.rs
+++ exercises/01_intro/08_outro/src/lib.rs
@@ -2,3 +2,31 @@
 //   a list containing the `k` largest numbers in the list, in descending order.
 //
 // Hint: you can use the `num_bigint` crate if you think it'd be useful.
+use num_bigint::BigInt;
+use pyo3::{
+    exceptions::{PyTypeError, PyValueError},
+    prelude::*,
+};
+
+#[pyfunction]
+fn max_k(mut v: Vec<BigInt>, k: usize) -> PyResult<Vec<BigInt>> {
+    if k > v.len() {
+        return Err(PyValueError::new_err(
+            "k should not exceed the length of list",
+        ));
+    }
+    if v.iter().any(|x| x < &BigInt::from(0)) {
+        return Err(PyTypeError::new_err("All number should be positive"));
+    }
+
+    v.sort();
+    v.reverse();
+    v.truncate(k);
+    Ok(v)
+}
+
+#[pymodule]
+fn outro1(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(max_k, m)?)?;
+    Ok(())
+}
