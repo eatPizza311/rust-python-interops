699ef65a0895005ce641bdb360971c8686832be2
ChihYing <spacetime0311@gmail.com>
2025-02-23 17:13:46 +0800

    feat: complete 07_execptions

--- exercises/01_intro/07_exceptions/src/lib.rs
+++ exercises/01_intro/07_exceptions/src/lib.rs
@@ -1,10 +1,25 @@
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
+use pyo3::types::PyInt;
 
 #[pyfunction]
 // TODO: Implement a function that returns a list containing the first `n` numbers in Fibonacci's sequence.
 //  It must raise a `TypeError` if `n` is not an integer or if it is less than 0.
-fn fibonacci(/* n: TODO */) /* -> TODO */ {
-    todo!()
+fn fibonacci(n: Bound<'_, PyInt>) -> PyResult<Vec<u64>> {
+    let n = n
+        .extract::<usize>()
+        .map_err(|_| PyTypeError::new_err("The value must be a positive interger"))?;
+    let mut result: Vec<u64> = Vec::from([0, 1]);
+    if n < 2 {
+        Ok(result[..n].to_vec())
+    } else {
+        for _ in 2..n {
+            let len = result.len();
+            let next = result[len - 1] + result[len - 2];
+            result.push(next);
+        }
+        Ok(result)
+    }
 }
 
 #[pymodule]
