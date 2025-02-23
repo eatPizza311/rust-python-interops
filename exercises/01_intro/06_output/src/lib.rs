9d8c829e16e4dd122fe7116a26b50acecbc2dd6f
ChihYing <spacetime0311@gmail.com>
2025-02-23 16:54:21 +0800

    feat: complete 06_output

--- exercises/01_intro/06_output/src/lib.rs
+++ exercises/01_intro/06_output/src/lib.rs
@@ -1,9 +1,20 @@
 use pyo3::prelude::*;
+use pyo3::types::{PyInt, PyList};
 
 #[pyfunction]
 // TODO: Implement a function that returns a list containing the first `n` numbers in Fibonacci's sequence.
-fn fibonacci(/* n: TODO */) /* -> TODO */ {
-    todo!()
+fn fibonacci(n: u64) -> Vec<u64> {
+    let mut result = Vec::from([0, 1]);
+    if n < 2 {
+        return result[..n as usize].to_vec();
+    } else {
+        for i in 2..n {
+            let len = result.len();
+            let next = result[len - 1] + result[len - 2];
+            result.push(next);
+        }
+    }
+    result
 }
 
 #[pymodule]
