77042fa1507ff7f3dc3b57e827c64570bff15a66
ChihYing <spacetime0311@gmail.com>
2025-02-28 13:21:09 +0800

    feat: complete 01_constructor

--- exercises/02_classes/01_constructors/src/lib.rs
+++ exercises/02_classes/01_constructors/src/lib.rs
@@ -1,4 +1,5 @@
-use pyo3::prelude::*;
+use pyo3::types::PyInt;
+use pyo3::{exceptions::PyValueError, prelude::*};
 
 // TODO: Add a `__new__` constructor to the `ShoppingOrder` class that takes the following arguments:
 //  - `name` (non-empty string)
@@ -14,6 +15,33 @@ struct ShoppingOrder {
     price: u64,
     #[pyo3(get, set)]
     quantity: u64,
+}
+
+#[pymethods]
+impl ShoppingOrder {
+    #[new]
+    fn new(name: String, price: Bound<'_, PyInt>, quantity: Bound<'_, PyInt>) -> PyResult<Self> {
+        if name.trim().is_empty() {
+            return Err(PyValueError::new_err("name can't be empty"));
+        }
+        let price: u64 = price
+            .extract()
+            .map_err(|_| PyValueError::new_err("price should be non-negative"))?;
+        if price == 0 {
+            return Err(PyValueError::new_err("price should be non-zero"));
+        }
+        let quantity = quantity
+            .extract()
+            .map_err(|_| PyValueError::new_err("quantity should be non-negative"))?;
+        if quantity == 0 {
+            return Err(PyValueError::new_err("quantity should be non-zero"));
+        }
+        Ok(ShoppingOrder {
+            name,
+            price,
+            quantity,
+        })
+    }
 }
 
 #[pymodule]
