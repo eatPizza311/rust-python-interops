061daa569910acd1a10d1afab67cd3aa56361460
ChihYing <spacetime0311@gmail.com>
2025-03-01 11:37:15 +0800

    feat: complete 06_parent

--- exercises/02_classes/06_parent/src/lib.rs
+++ exercises/02_classes/06_parent/src/lib.rs
@@ -5,7 +5,53 @@
 //  directly. The `history` list should be initialized as an empty list.
 use pyo3::prelude::*;
 
+#[pyclass(subclass)]
+struct Account {
+    #[pyo3(get, set)]
+    balance: f64,
+}
+
+#[pymethods]
+impl Account {
+    #[new]
+    fn new(balance: f64) -> Self {
+        Self { balance }
+    }
+}
+
+#[pyclass(extends=Account)]
+struct AccountWithHistory {
+    #[pyo3(get)]
+    history: Vec<f64>,
+}
+
+#[pymethods]
+impl AccountWithHistory {
+    #[new]
+    fn new(balance: f64) -> PyClassInitializer<Self> {
+        let account = Account::new(balance);
+        let account_history = AccountWithHistory {
+            history: Vec::new(),
+        };
+        PyClassInitializer::from(account).add_subclass(account_history)
+    }
+
+    #[getter]
+    fn balance(self_: PyRef<'_, Self>) -> f64 {
+        self_.as_super().balance
+    }
+
+    #[setter]
+    fn set_balance(mut self_: PyRefMut<'_, Self>, balance: f64) {
+        let curr_balance = self_.as_super().balance;
+        self_.history.push(curr_balance);
+        self_.as_super().balance = balance
+    }
+}
+
 #[pymodule]
 fn parent(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Account>()?;
+    m.add_class::<AccountWithHistory>()?;
     Ok(())
 }
