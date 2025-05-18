17f5f667b73f9e3015c8d7fc0982192f089b5de6
ChihYing <spacetime0311@gmail.com>
2025-05-18 12:46:37 +0800

    feat: extract internal links from HTML with normalization

--- exercises/03_concurrency/06_outro/src/lib.rs
+++ exercises/03_concurrency/06_outro/src/lib.rs
@@ -1,4 +1,7 @@
 use pyo3::{prelude::*, types::PySet};
+use scraper::{Html, Selector};
+use std::collections::HashSet;
+use url::Url;
 
 #[pyfunction]
 /// Given a starting URL (`start_from`), discover all the URLs *on the same domain*
@@ -47,8 +50,64 @@ pub fn site_map(start_from: String, site_map: Bound<'_, PySet>) {
     todo!()
 }
 
+fn craw_with_mock(url: &Url, html: &str) -> HashSet<String> {
+    let mut result = HashSet::new();
+    let document = Html::parse_document(html.trim());
+    let selector = Selector::parse(r#"a[href]"#).unwrap();
+
+    for element in document.select(&selector) {
+        if let Some(href) = element.value().attr("href") {
+            if let Ok(joined) = url.join(href) {
+                if joined.domain() == url.domain() {
+                    let clean = normalize_url(&joined);
+                    result.insert(clean);
+                }
+            };
+        }
+    }
+    result
+}
+
+fn normalize_url(url: &Url) -> String {
+    let mut url = url.clone();
+    url.set_fragment(None);
+    url.set_query(None);
+    url.to_string()
+}
+
 #[pymodule]
 fn outro3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(site_map, m)?)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_crawls_links_on_same_page() {
+        let html = r#"
+            <html>
+                <body>
+                    <a href="/about">About</a>
+                    <a href="http://example.com/contact">Contact</a>
+                    <a href="http://example.com/contact#section">Section</a>
+                    <a href="http://other.com">External</a>
+                </body>
+            </html>
+        "#;
+
+        let base_url = Url::parse("http://example.com").unwrap();
+
+        let result = craw_with_mock(&base_url, html);
+
+        let expected: HashSet<String> =
+            vec!["http://example.com/about", "http://example.com/contact"]
+                .into_iter()
+                .map(String::from)
+                .collect();
+
+        assert_eq!(result, expected);
+    }
 }
